b3ec4595d744b39309c95e2ca3e3289d05fe71bb
pierciccio <67201445+pierciccio@users.noreply.github.com>
2020-11-23 13:00:41 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
+import { AuthInterceptService } from './services/http/auth-intercept.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ArticoliComponent } from './components/articoli/articoli.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NewartComponent } from './components/newart/newart.component';
+import { ForbiddenComponent } from './components/forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    ArticoliComponent,
+    NavbarComponent,
+    FooterComponent,
+    JumbotronComponent,
+    LogoutComponent,
+    NewartComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: AuthInterceptService, multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
