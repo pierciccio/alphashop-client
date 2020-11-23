4437620a78d845557a0b677aaf256ec4efd3f378
pierciccio <67201445+pierciccio@users.noreply.github.com>
2020-11-23 13:00:08 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { ForbiddenComponent } from './components/forbidden/forbidden.component';
+import { Ruoli } from './models/Ruoli';
+import { NewartComponent } from './components/newart/newart.component';
+import { RouteGuardService } from './services/route-guard.service';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ArticoliComponent } from './components/articoli/articoli.component';
+import { ErrorComponent } from './components/error/error.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'index', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome/:userId', component: WelcomeComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.utente] } },
+  { path: 'articoli', component: ArticoliComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.utente] } },
+  { path: 'articoli/:filter', component: ArticoliComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.utente] } },
+  { path: 'newart/:codart', component: NewartComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.amministratore] } },
+  { path: 'forbidden', component: ForbiddenComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
